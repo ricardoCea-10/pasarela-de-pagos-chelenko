bfbd4080de4172277269c83a388901f7ff3324de
ricardoCea <ricardo.cea@genstudents.org>
2024-10-23 15:21:49 -0300

    elimino comentarios innecesarios

--- Controller/home-controller.js
+++ Controller/home-controller.js
@@ -175,54 +175,7 @@ function main() {
 */
 // ############################################################################################
 
-/*    
-    // Ruta para manejar la respuesta de Transbank
-    app.all('/retorno-pago', async (req, res) => {
-       
-        let tokenWs2, tbkToken, tbkOrdenCompra, tbkIdSesion;
 
-        //imprimimos el body
-        console.log("El BODY: ", req.body);
-
-        // Capturamos las variables de ambas fuentes: req.query (GET) y req.body (POST)
-        tokenWs2 = req.body.token_ws || req.query.token_ws; // pago exitoso o rechazado, error en formulario (solo produccion)
-        tbkToken = req.body.TBK_TOKEN || req.query.TBK_TOKEN; // pago abortado, error en formulario (solo produccion)
-        tbkOrdenCompra = req.body.TBK_ORDEN_COMPRA || req.query.TBK_ORDEN_COMPRA; // pago abortado, Timeout, error en formulario (solo produccion)
-        tbkIdSesion = req.body.TBK_ID_SESION || req.query.TBK_ID_SESION; // pago abortado, Timeout, error en formulario (solo produccion)
-
-        // Mostrar los valores que llegaron para revisar en consola
-        console.log("Los datos recibidos son:");
-        console.log("tokenWs2:", tokenWs2, "tbkToken:", tbkToken, "tbkOrdenCompra:", tbkOrdenCompra, "tbkIdSesion:", tbkIdSesion);
-
-        try {
-            // Si existe token_ws, la transacción fue exitosa o rechazada
-            if (tokenWs2) {
-                await confirmTransaction(tokenWs2); // Confirmar la transacción con el token_ws
-                console.log('Transacción correcta. El pago ha sido aprobado o rechazado.');
-                res.send('El pago ha sido aprobado o rechazado.');
-            } 
-            // Si existe TBK_TOKEN, TBK_ORDEN_COMPRA y TBK_ID_SESION, el pago fue abortado
-            else if (tbkToken && tbkOrdenCompra && tbkIdSesion) {
-                await confirmTransaction(tbkToken); // Confirmar transacción (abortada) con TBK_TOKEN
-                console.log('Transacción abortada.');
-                res.send('Transacción abortada. Pago rechazado.');
-            } 
-            // Si existe TBK_ORDEN_COMPRA y TBK_ID_SESION, la transacción ha excedido el tiempo (timeout)
-            else if (tbkOrdenCompra && tbkIdSesion) {
-                console.log('Transacción abortada por timeout.');
-                res.send('Transacción abortada. Timeout.');
-            } 
-            // Si no se encuentra ninguna variable, indicar un error
-            else {
-                console.log('Error en el proceso de pago. No se encontraron parámetros.');
-                res.status(400).send('Error en el proceso de pago. No se encontraron parámetros.');
-            }
-        } catch (error) {
-            console.error('Error al confirmar la transacción:', error);
-            res.status(500).send('Error en el servidor al procesar el pago.');
-        }
-    });
-*/
 
     // Ruta para mostrar la pantalla de pago rechazado
     app.get('/pago-rechazado', (req, res) => {
