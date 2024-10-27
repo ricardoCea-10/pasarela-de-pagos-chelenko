4933a99f714f2f5499b8e7ac88aa67570b3a79b5
ricardoCea <ricardo.cea@genstudents.org>
2024-10-26 22:07:26 -0300

    anado try | catch a modelos crear y confirmar tranasaccion

--- src/Model/crear-transaccion.js
+++ src/Model/crear-transaccion.js
@@ -7,23 +7,29 @@ const amount = 9999; // Este es el monto que estás cobrando
 const returnUrl = "http://localhost:3000/retorno"; // Esta es la URL a la que redirige Transbank después del pago
 
 async function createTransaction() {
-    const tx = new WebpayPlus.Transaction(
-        new Options(IntegrationCommerceCodes.WEBPAY_PLUS, IntegrationApiKeys.WEBPAY, Environment.Integration)
-    );
-    const response = await tx.create(buyOrder, sessionId, amount, returnUrl);
-
-    const tokenWs = response.token;
-    const formAction = response.url;
-
-    console.log("");
-    console.log("Datos de la creación de la transaccion:");
-    console.log("amount:", amount);
-    console.log("buyOrder:", buyOrder);
-    console.log("tokenWs:", tokenWs);
-    console.log("formAction:", formAction);
-    console.log("");
-
-    return { amount, buyOrder, tokenWs, formAction };
+    try {
+        // metodo de Transback para crear transacción (usamos SDK):
+        const tx = new WebpayPlus.Transaction(
+            new Options(IntegrationCommerceCodes.WEBPAY_PLUS, IntegrationApiKeys.WEBPAY, Environment.Integration)
+        );
+        const response = await tx.create(buyOrder, sessionId, amount, returnUrl);
+    
+        const tokenWs = response.token;
+        const formAction = response.url;
+    
+        console.log("");
+        console.log("Datos de la creación de la transaccion:");
+        console.log("amount:", amount);
+        console.log("buyOrder:", buyOrder);
+        console.log("tokenWs:", tokenWs);
+        console.log("formAction:", formAction);
+        console.log("");
+    
+        return { amount, buyOrder, tokenWs, formAction };
+        
+    } catch (error) {
+        console.error("Error al crear la transacción:", error);
+    }
 }
 
 export default createTransaction;
